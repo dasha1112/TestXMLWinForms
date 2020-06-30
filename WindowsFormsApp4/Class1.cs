32373c5e8cd282932f99d1b841dc755fef39b5f2
Даша <@DESKTOP-RN43QFF>
2020-06-30 21:51:46 +0300

    Первая версия

---
+++ WindowsFormsApp4/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMlproect
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public bool OnHoliday { get; private set; }
+
+        public Employee(string Name, int Age, bool OnHoliday)
+        {
+            Name = this.Name;
+            Age = this.Age;
+            OnHoliday = this.OnHoliday;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using XMlproect;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadEmloyees();
+        }
+
+        public void LoadEmloyees()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load("XMLFile2.xml");
+
+            foreach (XmlNode node in xml.DocumentElement)
+            {
+                string Name = node.Attributes[0].Value;
+                int Age = int.Parse(node["Age"].InnerText);
+                bool OnHoliday = bool.Parse(node["OnHoliday"].InnerText);
+                listBox1.Items.Add(new Employee (Name, Age, OnHoliday));
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != 1)
+            {
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
+            }
+        }
+    }
+}
